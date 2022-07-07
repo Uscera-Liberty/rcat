3fa36447ee947855d69f23b257dfb005b4acaf81
Maslyaeva Yulia <maslyayeva.yuliya@bk.ru>
2022-07-07 16:36:13 +0300

    db done)

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 from flask import Flask , render_template
 from wtf_formfields import *
+from models import *
+
 
 #configure app
 app = Flask(__name__)
 app.secret_key = 'replace later'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C://Users//admin//PycharmProjects//CatsBlog(flask_python)//database.db'
 
 @app.route('/' , methods = ['GET' , 'POST'])
 def index():
     reg_form = RegistrationForm()
     if reg_form.validate_on_submit():
-        return "success"
+        username = reg_form.username.data
+        password = reg_form.password.data
+
+        user_object = User.query.filter_by(username=username).first()
+        if user_object:
+            return "Someone else take this surname)"
+        user = User(username=username , password=password)
+        db.session.add(user)
+        db.session.commit()
+        return "Insertion complete"
+
     return render_template("index.html" , form = reg_form)
 
 
