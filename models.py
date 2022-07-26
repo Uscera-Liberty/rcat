a85b5dbe9346dac725caa837ad60e3a9dc67d8ad
Maslyaeva Yulia <maslyayeva.yuliya@bk.ru>
2022-07-26 18:45:39 +0300

    adding css + JS

--- models.py
+++ models.py
@@ -1,10 +1,13 @@
-from flask_login import UserMixin
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
 db = SQLAlchemy()
 
-class User(UserMixin , db.Model):
-    """User model"""
-    __tablename__ = 'user'
+
+class User(UserMixin, db.Model):
+    """ User model """
+
+    __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
-    username =db.Column(db.String(25) , unique = True ,nullable=True)
-    password = db.Column(db.String(), nullable=False)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    hashed_pswd = db.Column(db.String(), nullable=False)
